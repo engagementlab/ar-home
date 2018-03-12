2c432c84036977a6ad700fb1cc9b9e29f257315e
johnnycrich <johnny.c.rich@gmail.com>
2018-03-12 17:59:50 -0400

    Added moveable object script, working level reset.

---
+++ Assets/Scripts/Moveable.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+/// <summary>
+/// The Placeable class implements the logic used to determine if a GameObject
+/// can be placed on a target surface. Constraints for placement include:
+/// * No part of the GameObject's box collider impacts with another object in the scene
+/// * The object lays flat (within specified tolerances) against the surface
+/// * The object would not fall off of the surface if gravity were enabled.
+/// This class also provides the following visualizations.
+/// * A transparent cube representing the object's box collider.
+/// * Shadow on the target surface indicating whether or not placement is valid.
+/// </summary>
+public class Moveable : MonoBehaviour, IInputClickHandler
+{
+    [Tooltip("The base material used to render the bounds asset when placement is allowed.")]
+    public Material PlaceableBoundsMaterial = null;
+
+    [Tooltip("The base material used to render the bounds asset when placement is not allowed.")]
+    public Material NotPlaceableBoundsMaterial = null;
+
+    [Tooltip("The material used to render the placement shadow when placement it allowed.")]
+    public Material PlaceableShadowMaterial = null;
+
+    [Tooltip("The material used to render the placement shadow when placement it not allowed.")]
+    public Material NotPlaceableShadowMaterial = null;
+
+    [Tooltip("The type of surface on which the object can be placed.")]
+    public PlacementSurfaces PlacementSurface = PlacementSurfaces.Horizontal;
+
+    [Tooltip("The child object(s) to hide during placement.")]
+    public List<GameObject> ChildrenToHide = new List<GameObject>();
+
+    /// <summary>
+    /// Indicates if the object is in the process of being placed.
+    /// </summary>
+    public bool IsPlacing { get; private set; }
+
+    // The most recent distance to the surface.  This is used to 
+    // locate the object when the user's gaze does not intersect
+    // with the Spatial Mapping mesh.
+    private float lastDistance = 2.0f;
+
+    // The distance away from the target surface that the object should hover prior while being placed.
+    private float hoverDistance = 0.15f;
+
+    // Threshold (the closer to 0, the stricter the standard) used to determine if a surface is flat.
+    private float distanceThreshold = 0.02f;
+
+    // Threshold (the closer to 1, the stricter the standard) used to determine if a surface is vertical.
+    private float upNormalThreshold = 0.9f;
+
+    // Maximum distance, from the object, that placement is allowed.
+    // This is used when raycasting to see if the object is near a placeable surface.
+    private float maximumPlacementDistance = 5.0f;
+
+    // Speed (1.0 being fastest) at which the object settles to the surface upon placement.
+    private float placementVelocity = 0.06f;
+
+    // Indicates whether or not this script manages the object's box collider.
+    private bool managingBoxCollider = false;
+
+    // The box collider used to determine of the object will fit in the desired location.
+    // It is also used to size the bounding cube.
+    private BoxCollider boxCollider = null;
+
+    // Visible asset used to show the dimensions of the object. This asset is sized
+    // using the box collider's bounds.
+    private GameObject boundsAsset = null;
+
+    // Visible asset used to show the where the object is attempting to be placed.
+    // This asset is sized using the box collider's bounds.
+    private GameObject shadowAsset = null;
+
+    // The location at which the object will be placed.
+    private Vector3 targetPosition;
+
+    /// <summary>
+    /// Called when the GameObject is created.
+    /// </summary>
+    private void Awake()
+    {
+        targetPosition = gameObject.transform.position;
+
+        // Get the object's collider.
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+        if (boxCollider == null)
+        {
+            // The object does not have a collider, create one and remember that
+            // we are managing it.
+            managingBoxCollider = true;
+            boxCollider = gameObject.AddComponent<BoxCollider>();
+            boxCollider.enabled = false;
+        }
+    }
+
+    /// <summary>
+    /// Called when our object is selected.  Generally called by
+    /// a gesture management component.
+    /// </summary>
+    public void OnSelect()
+    {
+        /* TODO: 4.a CODE ALONG 4.a */
+
+        if (!IsPlacing)
+        {
+            OnPlacementStart();
+        }
+        else
+        {
+            OnPlacementStop();
+        }
+    }
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        /* TODO: 4.a CODE ALONG 4.a */
+
+        if (IsPlacing)
+        {
+            // Move the object.
+            Move();
+        }
+    }
+
+    /// <summary>
+    /// Put the object into placement mode.
+    /// </summary>
+    public void OnPlacementStart()
+    {
+        // If we are managing the collider, enable it. 
+        if (managingBoxCollider)
+        {
+            boxCollider.enabled = true;
+        }
+
+        // Hide the child object(s) to make placement easier.
+        for (int i = 0; i < ChildrenToHide.Count; i++)
+        {
+            ChildrenToHide[i].SetActive(false);
+        }
+        
+        // Enter placement mode.
+        IsPlacing = true;
+    }
+
+    /// <summary>
+    /// Take the object out of movement mode.
+    /// </summary>
+    public void OnPlacementStop()
+    {
+        IsPlacing = false;
+    }
+
+    /// <summary>
+    /// Positions the object along the surface toward which the user is gazing.
+    /// </summary>
+    /// <remarks>
+    /// If the user's gaze does not intersect with a surface, the object
+    /// will remain at the most recently calculated distance.
+    /// </remarks>
+    private void Move()
+    {
+        Vector3 moveTo = gameObject.transform.position;
+        Vector3 surfaceNormal = Vector3.zero;
+        RaycastHit hitInfo;
+
+        bool hit = Physics.Raycast(Camera.main.transform.position,
+            Camera.main.transform.forward,
+            out hitInfo,
+            20f,
+            1 >> 31);
+
+        if (hit)
+        {
+            float offsetDistance = hoverDistance;
+
+            // Place the object a small distance away from the surface while keeping 
+            // the object from going behind the user.
+            if (hitInfo.distance <= hoverDistance)
+            {
+                offsetDistance = 0f;
+            }
+
+            moveTo = hitInfo.point + (offsetDistance * hitInfo.normal);
+
+            lastDistance = hitInfo.distance;
+            surfaceNormal = hitInfo.normal;
+        }
+        else
+        {
+            // The raycast failed to hit a surface.  In this case, keep the object at the distance of the last
+            // intersected surface.
+            moveTo = Camera.main.transform.position + (Camera.main.transform.forward * lastDistance);
+        }
+
+        // Follow the user's gaze.
+        float dist = Mathf.Abs((gameObject.transform.position - moveTo).magnitude);
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, moveTo, placementVelocity / dist);
+
+        // Orient the object.
+        // We are using the return value from Physics.Raycast to instruct
+        // the OrientObject function to align to the vertical surface if appropriate.
+//        OrientObject(hit, surfaceNormal);
+    }
+
+    /// <summary>
+    /// Called when the GameObject is unloaded.
+    /// </summary>
+    private void OnDestroy()
+    {
+        // Unload objects we have created.
+        Destroy(boundsAsset);
+        boundsAsset = null;
+        Destroy(shadowAsset);
+        shadowAsset = null;
+    }
+
+    public virtual void OnInputClicked(InputClickedEventData e)
+    {
+        OnSelect();
+    }
+}
--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -18,9 +18,14 @@ public class SceneManagement : MonoBehaviour
 
 	// Use this for initialization
 	void Start () {
-		
+		//if (keywordRecognizer.Keywords.Any()) return;
 		keywords.Add("Start Over", () =>
 		{
+			Vuforia.CameraDevice.Instance.Deinit();
+			Vuforia.CameraDevice.Instance.Stop();
+			Vuforia.TrackerManager.Instance.GetTracker<Vuforia.ObjectTracker>().Stop();
+			Vuforia.DigitalEyewearARController.Instance.SetViewerActive(true, true);
+			
 			loadingImage.gameObject.SetActive(true);
 			StartCoroutine(RestartScene());
 		});
@@ -29,19 +34,25 @@ public class SceneManagement : MonoBehaviour
 			syncObjectSpawner.SpawnBasicSyncObject();
 			Debug.Log("Spawn object");
 		});
-		
+
 		// Tell the KeywordRecognizer about our keywords.
 		keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
 
 		// Register a callback for the KeywordRecognizer and start recognizing!
 		keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
 		keywordRecognizer.Start();
-		
+
+	}
+
+	public void Restart()
+	{
+		loadingImage.gameObject.SetActive(true);
+		StartCoroutine(RestartScene());
 	}
 
 	IEnumerator RestartScene()
 	{
-		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
 
 		//Wait until the last operation fully loads to return anything
 		while (!asyncLoad.isDone)
