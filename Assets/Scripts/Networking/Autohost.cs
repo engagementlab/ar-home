286065dde17e632b866802900ed9cb6fe4ee36cd
Stephanie Marker <Stephanie.Marker93@gmail.com>
2018-04-04 16:59:57 -0400

    Revert "Add network discovery scene. (#2)" (#3)
    This reverts commit 5053692bf5136818f24c11654e4b25e7f04f5130.

---
+++ Assets/Scripts/Networking/Autohost.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NetworkManager))]
+public class Autohost : MonoBehaviour
+{
+    private void Start()
+    {
+        this.GetComponent<NetworkManager>().StartHost();
+    }
+}
