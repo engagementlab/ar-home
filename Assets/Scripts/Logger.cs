5b64fedd2e79bfb46d87459eedee98fcdc1ce214
Gavin <gavinbauman1322@gmail.com>
2018-04-06 15:10:49 -0400

    Add DebugUtil and Logger Script

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -1,18 +1,33 @@
-﻿using UnityEngine;
+﻿using HoloToolkit.Unity.SharingWithUNET;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Logger : MonoBehaviour
 {
-    TextMesh textMesh;
+    Text textMesh;
+    public Transform AnchorPos;
+    public NetworkManager NetManager;
+    public UNetAnchorManager AnchorManager;
+
+
+
+    
 
     // Use this for initialization
     void Start()
     {
-        textMesh = gameObject.GetComponentInChildren<TextMesh>();
+        textMesh = gameObject.GetComponentInChildren<Text>();
+        //Application.logMessageReceived += LogMessage;
+
     }
+
     private void Update()
     {
-        Application.logMessageReceived += LogMessage;
-
+        textMesh.text = "Anchor Position: " + AnchorPos.position.ToString() + "\n";
+        textMesh.text += "Number of Players: " + NetManager.numPlayers.ToString() + "\n";
+        textMesh.text += "Net Address: " + NetManager.networkAddress.ToString() + "\n";
+        textMesh.text += "Is Anchor Esatblished: " + AnchorManager.AnchorEstablished.ToString() + "\n";
     }
 
     public void LogMessage(string message, string stackTrace, LogType type)
