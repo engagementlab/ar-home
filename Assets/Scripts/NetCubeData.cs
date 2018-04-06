c434df6877e62d45125bc179636cadcd39ddedb2
bengreenier <ben@bengreenier.com>
2018-04-06 08:48:57 -0400

    initial integration pass

---
+++ Assets/Scripts/NetCubeData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+    public class NetCubeData : NetworkBehaviour
+    {
+        [SyncVar(hook = "OnTimeSinceSpawnChanged")]
+        private float timeSinceSpawn = 0;
+
+        private void OnTimeSinceSpawnChanged(float newTimeValue)
+        {
+            if (isClient && !hasAuthority)
+            {
+                Debug.Log("Got NetCubeData.timeSinceSpawn update: " + newTimeValue + " (" + timeSinceSpawn + ")");
+            }
+        }
+
+        private void Update()
+        {
+            if (hasAuthority)
+            {
+                timeSinceSpawn += Time.deltaTime;
+            }
+        }
+    }
+}
